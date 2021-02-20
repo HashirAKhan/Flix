c12321b1947c7843be7f6caf3ce91ae6a98bfe54
Hashir Khan <hkhan6265@bths.edu>
2021-02-19 19:18:57 -0500

    Completed main assignment

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -37,15 +37,9 @@ class MovieDetailsViewController: UIViewController {
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let movieTrailerView = segue.destination as! MovieTrailerView
+        movieTrailerView.movie = movie
     }
-    */
 
 }
--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -14,12 +14,17 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     var movies = [[String:Any]]()
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
         
-        
-
-        // Do any additional setup after loading the view.
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -32,24 +37,29 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 // TODO: Get the array of movies
                 self.movies = (dataDictionary["results"] as? [[String:Any]])!
-                // TODO: Store the movies in a property to use elsewhere
-                // TODO: Reload your table view data
                 self.collectionView.reloadData()
-                print(self.movies)
             }
         }
         task.resume()
-        
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.collectionView.reloadData()
         
     }
+ 
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return movies.count
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
         
-        let movie = self.movies[indexPath.row]
+        let movie = self.movies[indexPath.item]
         let baseURL = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterURL = URL(string: baseURL + posterPath)
@@ -57,14 +67,15 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         return cell
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        print("movie selected")
+        //Find out which movie was selected
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.item]
+        //send movie information
+        let movieDetailedPage = segue.destination as! MovieDetailsViewController
+        movieDetailedPage.movie = movie
     }
-    */
 
 }
